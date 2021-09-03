194c30131a2ee893fe36a1184531e52defa51b57
vincent-guo <vincent.weiguo@gmail.com>
2021-09-04 00:19:07 +0800

    12 Binding

---
+++ DesignCode/HomeView.swift
@@ -0,0 +1,35 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Vincent Guo on 2021/9/4.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.top, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
