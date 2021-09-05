ad1e2cb2073b7154cfa22cbe8a98719102abeb46
vincent-guo <vincent.weiguo@gmail.com>
2021-09-05 23:21:35 +0800

    18 NavigationStyle

---
+++ DesignCode/UpdateDetail.swift
@@ -0,0 +1,33 @@
+//
+//  UpdateDetail.swift
+//  DesignCode
+//
+//  Created by Vincent Guo on 2021/9/5.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    var update: Update = updateData[1]
+    
+    var body: some View {
+        List {
+            VStack(spacing: 20) {
+                Image(update.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                Text(update.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .navigationBarTitle(update.title)
+        }
+        .listStyle(PlainListStyle())
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
