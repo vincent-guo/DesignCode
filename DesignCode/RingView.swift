cf621a40a06a1c7694ed8edb211bcdddef69fb2d
vincent-guo <vincent.weiguo@gmail.com>
2021-09-07 16:59:38 +0800

    23 RingAnimation

--- DesignCode/RingView.swift
+++ DesignCode/RingView.swift
@@ -13,6 +13,7 @@ struct RingView: View {
     var width: CGFloat = 300
     var height: CGFloat = 300
     var percent: CGFloat = 88
+    @Binding var show: Bool
     
     var body: some View {
         let multiplier = width / 44
@@ -24,7 +25,7 @@ struct RingView: View {
                 .frame(width: width, height: height)
             
             Circle()
-                .trim(from: progress, to: 1)
+                .trim(from: show ? progress : 1, to: 1)
                 .stroke(
                     LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
                     style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0))
@@ -39,12 +40,15 @@ struct RingView: View {
             Text("\(Int(percent))%")
                 .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
+                .onTapGesture {
+                    self.show.toggle()
+                }
         }
     }
 }
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(show: .constant(true))
     }
 }
